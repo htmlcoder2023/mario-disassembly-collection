884e13a019bc645403150950e6236695d70beedc
htmlcoder2023 <129872277+htmlcoder2023@users.noreply.github.com>
2023-12-31 12:40:46 -0600

    Create assemble.bat

---
+++ Disassemblies/smb2j disassembly/assemble.bat
@@ -0,0 +1,10 @@
+del sm2data2.bin
+del sm2data3.bin
+del sm2data4.bin
+del sm2main.bin
+del smb2.fds
+asm6 sm2data2.asm
+asm6 sm2data3.asm
+asm6 sm2data4.asm
+asm6 sm2main.asm
+asm6 fdswrap.asm smb2.fds
